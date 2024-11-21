10d63ee436449779dfb86df97692187854e0bc39
Sergio Ramirez <sergiodanramirez@gmail.com>
2024-11-21 18:07:59 -0300

    cambiazo

--- src/components/CardCategory.jsx
+++ src/components/CardCategory.jsx
@@ -7,7 +7,7 @@ function CardCategory({ name, icon, colorIcon}) {
         <div
             className="shadow-md flex items-center bg-gray-200 :bg-gray-800 p-2 transform transition-all duration-300 hover:scale-110 cursor-pointer"
         >
-            <Link href="/app/busqueda" className="w-full">
+            <Link href="/busqueda" className="w-full">
                 <div className="flex items-center justify-between">
 
                     <p className="font-semibold">
